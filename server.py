c19a4e3c31e01993b8ddf2b563f1d24a15793b50
Ashraf Taifour <ashraftayfour@gmail.com>
2020-10-29 17:46:00 -0400

    added ClientHandler function

--- server.py
+++ server.py
@@ -15,6 +15,20 @@ server.bind((SERVER, PORT))  # passed as tuple
 def Init():  # this function will be called to initilize the server
     server.listen()
     print("Server is listening on " + SERVER)
+    while True:  # server will always be listening
+        # will wait for a connection and store address in addr and a socket object in conn
+        conn, addr = server.accept()
+        # will start a thread where the function ClientHandler will be used to handle the upcoming connection.
+        thread = threading.Thread(target=ClientHandler, args=(conn, addr))
+        thread.start()
+
+        # will display all active threads, we are subtracting 1 since we will always have 1 active thread at minimum even without connections
+        print("THE NUMBER OF ACTIVE CONNECTIONS IS CURRENTLY" +
+              threading.active_count() - 1)
+
+
+def ClientHandler(addr, conn):
+    print(addr + "has connected.")
 
 
 print("initializing server...")
