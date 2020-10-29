6de04a60b5ab5e13ec8cd7dbf5aae553ec7ee5f0
Ashraf Taifour <ashraftayfour@gmail.com>
2020-10-29 18:05:33 -0400

    server file is finished

--- server.py
+++ server.py
@@ -10,11 +10,13 @@ SERVER = socket.gethostbyname(HOST_NAME)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((SERVER, PORT))  # passed as tuple
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "disconnect!"
 
 
 def Init():  # this function will be called to initilize the server
     server.listen()
-    print("Server is listening on " + SERVER)
+    print("Job Creator is listening on " + SERVER)
     while True:  # server will always be listening
         # will wait for a connection and store address in addr and a socket object in conn
         conn, addr = server.accept()
@@ -29,6 +31,18 @@ def Init():  # this function will be called to initilize the server
 
 def ClientHandler(addr, conn):
     print(addr + "has connected.")
+    connected = True
+    while connected:
+        msg_len = conn.recv(HEADER).decode(FORMAT)
+        # code will stop here until a msg is received from the client, it will receive a HEADER number of bytes and it will decode the message from its bytes format to a string.
+        msg_len = int(msg_len)  # convert message length to an int
+        msg = conn.recv(msg_len).decode(FORMAT)
+        # displays message
+        print(addr + " has sent the following message: " + msg)
+        if msg == DISCONNECT_MSG:  # if client asks to disconnect it will disconnect
+            connected = False
+
+    conn.close()
 
 
 print("initializing server...")
