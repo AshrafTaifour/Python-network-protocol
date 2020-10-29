f70bd244ee0cf21b277c7a52b1fcc12e5f2dac0c
Ashraf Taifour <ashraftayfour@gmail.com>
2020-10-29 17:34:28 -0400

    created function for initializing the server

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 import socket
 import threading
 
-#The following variables are constants
-HEADER = 128 #Will contain information about message properties
-PORT = 1996 #Port that the socket will be using
-HOST_NAME = socket.gethostname() #will get the name of the machine
-SERVER = socket.gethostbyname(HOST_NAME) #will get the IP address by using the machine name
+# The following variables are constants
+HEADER = 128  # Will contain information about message properties
+PORT = 1996  # Port that the socket will be using
+HOST_NAME = socket.gethostname()  # will get the name of the machine
+# will get the IP address by using the machine name
+SERVER = socket.gethostbyname(HOST_NAME)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((SERVER, PORT)) #passed as tuple
+server.bind((SERVER, PORT))  # passed as tuple
 
+
+def Init():  # this function will be called to initilize the server
+    server.listen()
+    print("Server is listening on " + SERVER)
+
+
+print("initializing server...")
+Init()
