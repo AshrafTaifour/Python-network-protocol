83d215c46d9c1d733c89fed3411e62e60ea9087b
omar S <omarsabb1995@gmail.com>
2020-10-30 18:43:07 -0400

    put client handler first

--- server.py
+++ server.py
@@ -13,6 +13,21 @@ server.bind((SERVER, PORT))  # passed as tuple
 FORMAT = 'utf-8'
 DISCONNECT_MSG = "!disconnect"
 
+def ClientHandler(addr, conn):
+    print(f"Client {addr} has connected.")
+    connected = True
+    while connected:
+        msg_len = conn.recv(HEADER).decode(FORMAT)
+        if msg_length: #will ignore any messages with are 0, usually connection a 0 message is sent
+            msg_length = int(msg_length)
+            # code will stop here until a msg is received from the client, it will receive a HEADER number of bytes and it will decode the message from its bytes format to a string.
+            msg_len = int(msg_len)  # convert message length to an int
+            msg = conn.recv(msg_len).deco8de(FORMAT)
+            # displays message
+            print(f"{addr}  has sent the following message: {msg}")
+            if msg == DISCONNECT_MSG:  # if client asks to disconnect it will disconnect
+                connected = False
+    conn.close()
 
 def Init():  # this function will be called to initilize the server
     server.listen()
@@ -28,22 +43,6 @@ def Init():  # this function will be called to initilize the server
         print(f"THE NUMBER OF ACTIVE CONNECTIONS IS CURRENTLY  {threading.active_count() - 1}")
 
 
-def ClientHandler(addr, conn):
-    print(f" {addr} has connected.")
-    connected = True
-    while connected:
-        msg_len = conn.recv(HEADER).decode(FORMAT)
-        if msg_length: #will ignore any messages with are 0, usually connection a 0 message is sent
-            msg_length = int(msg_length)
-            # code will stop here until a msg is received from the client, it will receive a HEADER number of bytes and it will decode the message from its bytes format to a string.
-            msg_len = int(msg_len)  # convert message length to an int
-            msg = conn.recv(msg_len).deco8de(FORMAT)
-            # displays message
-            print(f"{addr}  has sent the following message: {msg}")
-            if msg == DISCONNECT_MSG:  # if client asks to disconnect it will disconnect
-                connected = False
-
-    conn.close()
 
 
 print("initializing server...")
